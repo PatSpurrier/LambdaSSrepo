6d79d9ba49cc43058f7a88864925d7037d9ab3fb
PatSpurrier <80483950+PatSpurrier@users.noreply.github.com>
2021-03-11 14:13:27 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,30 @@
+//Include all the screenshot parameters in this configuration file
+module.exports = { 
+     urllist: ["https://myworkday.com", "https://service-now.com", "https://microsoftonline.com", "https://powerapps.com", "https://yammer.com", "https://vmwareidentity.com", "https://concursolutions.com", "https://splunkcloud.com", "https://ibmcloud.com", "https://dfsco.com", "https://statestreet.com", "https://ibm.com", "https://office.com", "https://microsoft.com", "https://force.com", "https://financialengines.com", "https://wellsfargo.com", "https://nysaves.org", "https://unite529.com", "https://seismic.com", "https://bnymellon.com", "https://dynamics.com", "https://bwise.net", "https://e-invoice.com", "https://bbh.com", "https://yourcause.com", "https://adp.com", "https://thomson.com", "https://teammatehosting.com", "https://windows.net", "https://microsoftstream.com", "https://salesforce.com", "https://cticompliance.com", "https://qvidian.com", "https://crd.com", "https://onesourcelogin.com", "https://onesourcetax.com", "https://collegesavingsiowa.com", "https://workspaceoneaccess.com", "https://fnz.com", "https://jpmorgan.com", "https://msci.com", "https://trgrp.com", "https://vanguard.com", "https://adp.ca", "https://aprimo.com", "https://collegeinvest529.com", "https://concur.com", "https://live.com", "https://medallia.com", "https://ubs.com", "https://yourcauseuat.com", "https://barcap.com", "https://feitest.com", "https://office.net", "https://svc.ms", "https://vgcontent.info", "https://vgi529.com", "https://workday.com"],
+     BrowserConfigurations: {
+        "windows 10": {
+          "edge": [
+            "87",
+          ],
+          "chrome": [
+            "76",
+            "75"   
+          ],
+          "ie": [
+            "11"
+          ]
+        },
+        "windows 8": {
+          "ie": [
+            "10"
+          ]
+        },
+      },
+     defer_time: 3,
+     email: false,
+     layout: "portrait",
+     mac_res: "1024x768",
+     win_res: "1366X768",
+     smart_scroll: true
+};
+
